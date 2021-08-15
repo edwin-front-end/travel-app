c2964fe0f837ae79a36cf88b27c6ffb063a09049
Edwin Mensah <Eddy@MacBook-Pro-di-Edwin.local>
2021-08-15 20:56:16 +0100

    feat: add html structure and some css styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import "./client/styles/base.scss";
+import "./client/styles/input.scss";
 import "./client/styles/button.scss";
 import "./client/styles/form.scss";
 import "./client/styles/reset.scss";
