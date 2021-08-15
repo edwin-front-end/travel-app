ae8a6afa831b976f5be02b48f10151d7afe68ca9
Edwin Mensah <Eddy@MacBook-Pro-di-Edwin.local>
2021-08-15 11:48:20 +0100

    installed webpack and first configuration setup

--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,33 @@
+// Dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
-
+// create an instance of express
 const app = express();
-const port = 8080;
+// cors for cross origin allowance
+const cors = require("cors");
 const { request } = require("express");
-
-app.use(cors);
+app.use(cors());
+// configuring express to use body-parser as a middleware
 app.use(bodyParser.urlencoded({ extended: false }));
+// convert data to json
 app.use(bodyParser.json());
-
-app.listen(port, () => {
-    console.log("listening on port 8080");
+//initialize the main project folder
+app.use(express.static("website"));
+// run the server
+const port = 3000;
+app.listen(port, (req, res) => {
+    console.log(`server is running on port ${port}`);
 });
+// endpoint to store data
+let projectData = {};
+
+// request data to the openWeatherAPI
+// app.post("/weather", (req, res) => {
+//     projectData = req.body;
+//     console.log(projectData);
+// });
+
+// send the data received to projectData object
+// app.get("/data", (req, res) => {
+//     res.send(projectData);
+// });
