eeaf2fbbff84393d255557935f2089ad547febda
Edwin Mensah <Eddy@MacBook-Pro-di-Edwin.local>
2021-10-06 19:10:15 +0100

    test: added test post function

---
+++ __tests__/post.spec.js
@@ -0,0 +1,12 @@
+import "regenerator-runtime/runtime";
+
+import { postData } from "../src/client/js/postFunc";
+
+describe("testing the date countdown functionality", () => {
+    test("tests dateCountdown() function", () => {
+        //    input
+        // expected output
+        // expect()
+        expect(postData).toBeDefined();
+    });
+});
